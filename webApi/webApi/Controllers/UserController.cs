b0e1ef459377ac86be749bdc4d4990d4ee931f54
LoveAnd-Share <1836167892@qq.com>
2023-05-23 09:39:49 +0800

    新建工程

---
+++ webApi/webApi/Controllers/UserController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace webApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        [HttpGet]
+        public string Ret()
+        {
+            return "get";
+        }
+    }
+}
